d136ccdf29854cfb16a593e8a788dd9e070aac63
Marie <muramatsumarie@gmail.com>
2023-05-04 23:20:17 +0900

    add files changes

---
+++ Sound/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Sound
+//
+//  Created by Marie Muramatsu on 4/5/23.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    let drumSoundPlayer = try!AVAudioPlayer(data: NSDataAsset(name:"drumSound")!.data)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func tapDrumButton(){
+        drumSoundPlayer.currentTime = 0
+        
+        drumSoundPlayer.play()
+        
+        
+    }
+
+
+}
+
